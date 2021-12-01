3cf1490dc7bf9296e470cc536f1bf2875b4c6cce
Jamil Mohammed <zeus2tee@gmail.com>
2021-12-01 14:58:10 +0000

    added to git

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+// let App = () => {
+//   return (
+//     <div>
+//       <h1>Codetrain is awesome, react works!!!!</h1>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Codetrain is awesome, react works!!!!</h1>
+    )
+  }
+}
+
+export default App;
